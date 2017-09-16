f6b9895512a708f22dfaaffda2a74119e958783b
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-09-16 18:42:58 -0500

    i think its done

--- homework1.js
+++ homework1.js
@@ -222,26 +222,26 @@ def: 10,
 att: 3
 }
 const RNG = (min, max) => {
-	return Math.random() * (max - min) + min;
+	return Math.floor( Math.random() * (max - min) + min );
 }
 
-const battle = (one, two) => {}
-//	while ((one.hp > 0) || (two.hp > 0)) {
+const battle = (one, two) => {
+	while ((one.hp > 0) && (two.hp > 0)) {
 		//attack
 		if ((RNG(0,20) + one.att) > two.def ) { //scores a hit
 			const damage = RNG(one.damage_min, one.damage_max); //assign damage
-			console.log(one.name + " strikes " + two.name "for " + damage + " damage!!")
+			console.log(one.name + " strikes " + two.name + "for " + damage + " damage!!")
 			two.hp -= damage;
 		}
-		else { console.log(one.name + " swings wildly and misses!")} //miss
+		else { console.log(two.name + " dodges nimbly!")} //miss
 	//retaliate
-		if ((RNG(0,20) + one.att) > two.def ) { //scores a hit
+		if ((RNG(0,20) + two.att) > one.def ) { //scores a hit
 			const damage = RNG(two.damage_min, two.damage_max); //assign damage
-			console.log(one.name + " strikes " + two.name "for " + damage + " damage!!")
+			console.log(two.name + " strikes " + one.name + "for " + damage + " damage!!")
 			one.hp -= damage;
 		}
-		else { console.log(one.name + " swings wildly and misses!")} //miss
-//	}
+		else { console.log(one.name + " dodges nimbly!")} //miss
+	}
 if (one.hp < 1) {console.log(one.name + " dies.")}
-if (two.hp < 1) {console.log(one.name + " dies.")}
+if (two.hp < 1) {console.log(two.name + " dies.")}
 }
