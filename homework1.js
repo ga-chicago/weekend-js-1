9c5137bd7d485db1822faa7ee499cbaaeacbc3cc
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-09-16 17:10:04 -0500

    brb

---
+++ homework1.js
@@ -0,0 +1,75 @@
+let variable = 1;
+variable = 2;
+const partyAllNight = variable;
+
+let firstVariable = "Hello World";
+firstVariable = 4;
+let secondVariable = firstVariable;
+secondVariable = "It's me again";
+//value is 4;
+
+myName = "Todd Herskovitz";
+console.log("Hello my name is " + myName);
+
+a < b
+c > d
+'name' === 'name'
+a < b < c
+a === a < d
+e === 'kevin'
+48 !== '48'
+
+if (animal === "cow") {console.log("moooo");}
+else {console.log("u r not a cow");}
+
+age = 17;
+if (age >= 16) {console.log("here are the keys");}
+else {console.log("sorry too young");}
+
+for (let i = 0; i <= 10; i++) { console.log(i);}
+for (let i = 10; i <= 4000; i++) { console.log(i);}
+for (let i = 10; i <= 4000; i+=2) { console.log(i);}
+
+for (let i = 2; i <= 100; i+=2) { console.log(i + " is an even number");}
+
+
+for (let i = 5; i <= 100; i+=5) { console.log("i found a " + i + ". high five!");}
+
+for (let i = 0; i <= 100; i++) { 
+if ( i % 3 == 0) {console.log("this is " i + ". 3 is a crowd");}
+if ( i % 5 == 0) { console.log("i found a " + i + ". high five!");}x
+}
+
+const array = [1,2,3,4,5,6,7,8,9,10];
+bankAccount = array.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+});
+
+let accumulator = 0;
+for (let i = 0; i <= 100; i++) { accumulator += i;}
+bankAccount = accumulator * 2
+
+let accumulator = 0;
+for (let i = 0; i < 1000; i++) { 
+if ( i % 3 == 0) { accumulator += i;}
+if ( i % 5 == 0) { accumulator += i;}
+}
+
+
+
+
+const swashbuckler {
+hp: 30;
+damage_min: 3;
+damage_max: 10;
+def: 15;
+att: 5;
+}
+
+const barbarian {
+hp: 60;
+damage_min: 6;
+damage_max: 15;
+def: 10;
+att: 5;
+}
