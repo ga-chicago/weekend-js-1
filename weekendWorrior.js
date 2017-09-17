35f26c2fc60a1f296326a5de649a7a02265711ae
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-09-17 15:09:21 -0500

    finished a couple more

---
+++ weekendWorrior.js
@@ -0,0 +1,197 @@
+
+//1 Strings
+
+let firstVariable = "Hello World";
+
+
+parseInt(firstVariable)
+
+let secondVariable = firstVariable
+
+functionIForgotDoesThis(secondVariable)
+
+//-----> The value of the firstVariable would be the number we converted it to (11?)
+
+
+//2 Strings
+
+const yourName = 'Giovanni Perez'
+
+let finalAnswer =  return "hello, my name is " + yourName
+
+
+
+
+//3 Booleans
+
+const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+a !== b
+c !== d
+'Name' === 'Name'
+
+
+// The Farm
+
+const butcher = (animal) => {
+	if (animal === 'cow') {
+		return 'Moooooo!'
+	} else { return 'Hey! Youre not a cow'}
+}
+
+
+
+
+
+// Driver's Ed
+
+let age = 21;
+
+const dad = (age) => {
+	if(age > 16) {
+		alert('Here are the keys')
+	} else { 
+		alert('Sorry spud, youre too young')}
+}
+
+
+// Just Loop it! 
+//1
+
+for (let i = 0; i = -11; i--) {
+	return i
+}
+
+//2 
+
+for (let i = 10; i = -4001; i--) {
+	return i
+}
+
+//3
+for (let i = 10; i = -4001; i--) {
+	return i
+}
+
+//Let's Get Even, Steven
+
+
+for (let i = 0; i = -100; i--) {
+	if(i % 2 0){
+		return i + ' is an even number'
+	} else {
+		console.log('not today, satan!')
+	}
+}
+
+
+// Give Me Five NEEDS WORK
+
+for (let i = 0; i > -101; i--) {
+	if( i) {
+		return 'I found ' i + 'Hight five!!'
+	}
+}
+
+//Savings account NEEDS WORK 
+
+let bankAccount = [0];
+for(let i = 0; i = 11; i++){
+		bankAccount += i
+	}
+
+
+
+
+//Multiples of 3 and 5
+
+
+
+
+//Easy Does It
+
+let arr = ['Rikki-tikki-tavi', 'Wubba lubba dub dub', 'The Human Torch was denied a bank loan']
+
+
+//Random
+
+const randomThings = [1, 10, "Hello", true]
+
+
+randomThings[0]
+randomThings[2] = 'world'
+randomThings
+
+
+//We've Got Class
+
+const ourClass = ["Gizmo", "Zoom", "Github", "Slack"]
+
+
+ourClass[2]
+ourClass[2] = 'Octocat'
+ourClass.push('Cloud City')
+
+
+
+
+
+//Mix It Up
+
+
+
+
+
+
+
+//Biggie Smalls
+
+
+
+
+
+
+
+
+//Monkey in the Middle
+
+
+
+
+
+
+
+
+//What's in Your Closet?
+
+
+
+//Functions
+//printGreeting
+
+
+
+
+
+
+//reverseWordOrder
+
+
+
+
+
+//calculate
+
+
+
+
+
+
+
+
+
+
