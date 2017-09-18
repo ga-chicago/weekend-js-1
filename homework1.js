81906a486a54dbdb30392fb45b1198725ecd8479
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-09-17 21:06:08 -0500

    did some formatting

--- homework1.js
+++ homework1.js
@@ -36,8 +36,8 @@ for (let i = 2; i <= 100; i+=2) { console.log(i + " is an even number");}
 for (let i = 5; i <= 100; i+=5) { console.log("i found a " + i + ". high five!");}
 
 for (let i = 0; i <= 100; i++) { 
-if ( i % 3 == 0) {console.log("this is " i + ". 3 is a crowd");}
-if ( i % 5 == 0) { console.log("i found a " + i + ". high five!");}
+  if ( i % 3 == 0) {console.log("this is " + i + ". 3 is a crowd");}
+  if ( i % 5 == 0) { console.log("i found a " + i + ". high five!");}
 }
 
 const array = [1,2,3,4,5,6,7,8,9,10];
@@ -51,8 +51,8 @@ bankAccount = accumulator * 2
 
 let accumulator = 0;
 for (let i = 0; i < 1000; i++) { 
-if ( i % 3 == 0) { accumulator += i;}
-else if ( i % 5 == 0) { accumulator += i;}
+  if ( i % 3 == 0) { accumulator += i;}
+  else if ( i % 5 == 0) { accumulator += i;}
 }
 
 const randomThings = [1, 10, "Hello", true]
@@ -71,8 +71,8 @@ ourClass.reverse();
 if (variable < 100) {console.log("little number")} else {console.log("big number")}
 
 if (variable < 5) {console.log("little number")} 
-else if (variable > 10) {console.log("big number")}
-else {console.log("monkey")}
+  else if (variable > 10) {console.log("big number")}
+  else {console.log("monkey")}
 
 const kristynsCloset = [
   "left shoe",
@@ -117,56 +117,56 @@ thomsCloset[2][1]
 thomsCloset[1][2] = "footie pajamas"
 
 const printGreeting = (name) => {
-return "hello " + name + "."
+  return "hello " + name + "."
 }
 
 const reverseWordOrder = (string) => {
-return string.split(" ").reverse().join(" ");
+  return string.split(" ").reverse().join(" ");
 }
 
 const calculate = (num1, num2, operation) => {
-if (operation === "add") {return num1 + num2}
-if (operation === "sub") {return num1 - num2}
-if (operation === "mult") {return num1 * num2}
-if (operation === "div") {return num1 / num2}
-if (operation === "exp") {return Math.pow(4,3)}
+  if (operation === "add") {return num1 + num2}
+  if (operation === "sub") {return num1 - num2}
+  if (operation === "mult") {return num1 * num2}
+  if (operation === "div") {return num1 / num2}
+  if (operation === "exp") {return Math.pow(4,3)}
 }
 
 const printGreeting = (Name) => {
-return Name + " is cool."
+  return Name + " is cool."
 }
 
 const weCanCubeThat = (Number) => {
-return Math.pow(Number,3)
+  return Math.pow(Number,3)
 }
 
 const vowels = ["a","e","i","o","u","y","A","E","I","O","U","Y"];
 
 const isAVowel = (Letter) => {
-if ( vowels.indexOf(Letter) >= 0 ) {return "Yeah that's a vowel."}
-else {return "oh that? naw that's not a vowel"}
+  if ( vowels.indexOf(Letter) >= 0 ) {return "Yeah that's a vowel."}
+  else {return "oh that? naw that's not a vowel"}
 }
 
 
 const stringLength = (String1, String2) => {
-return [String1.length, String2.length]
+  return [String1.length, String2.length]
 }
 
 const multipleStringLength = (array) => {
-let lengths = [];
-for (i = 0; i < array.length; i ++) {lengths.push(array[i].length);}
-return lengths
+  let lengths = [];
+  for (i = 0; i < array.length; i ++) {lengths.push(array[i].length);}
+  return lengths
 }
 
 const maxOfThree = (one, two, three) => {
-function compareNumbers(a, b) {return b - a;}
-let array = [one, two, three];
-return array.sort(compareNumbers)[0];
+  function compareNumbers(a, b) {return b - a;}
+  let array = [one, two, three];
+  return array.sort(compareNumbers)[0];
 }
 
 const longest = (array) => {
-function compareLength(a, b) {return b.length - a.length;}
-return array.sort(compareLength)[0];
+  function compareLength(a, b) {return b.length - a.length;}
+  return array.sort(compareLength)[0];
 }
 
 
@@ -175,14 +175,14 @@ const transmogrifier = (one, two, three) => {
 };
 
 const object = {		// make an unchanging object called object initialize with =,  opens with braces
-property1: 69,			// add property called property1 which is a number 69, next with commq
-property2: "whatever"	//add property called property2 which is a string 'whatever' end with semicolon
-}						// close with braces
+  property1: 69,			// add property called property1 which is a number 69, next with commq
+  property2: "whatever"	//add property called property2 which is a string 'whatever' end with semicolon
+}	  	 				// close with braces
 
 const me = {
-name: "Todd",
-age: 27,
-email: "todd.hersko@gmail.com"
+  name: "Todd",
+  age: 27,
+  email: "todd.hersko@gmail.com"
 }
 
 me.name;
@@ -205,27 +205,27 @@ console.log(monster.age);
 
 
 const swashbuckler = {
-name: "Reckless Swashbuckler",
-hp: 300,
-damage_min: 10,
-damage_max: 30,
-def: 15,
-att: 7
+  name: "Reckless Swashbuckler",
+  hp: 300,
+  damage_min: 10,
+  damage_max: 30,
+  def: 15,
+  att: 7
 }
 
 const knight = {
-name: "Noble Knight",
-hp: 400,
-damage_min: 40,
-damage_max: 50,
-def: 10,
-att: 3
+  name: "Noble Knight",
+  hp: 400,
+  damage_min: 40,
+  damage_max: 50,
+  def: 10,
+  att: 3
 }
 const RNG = (min, max) => {
 	return Math.floor( Math.random() * (max - min) + min );
 }
-//setTimeout(function(){ reshuffle(); }, timer)
-const battle = (one, two) => {
+
+const battle = (one, two) => {      // this is going to look ugly i think no matter how i format this - it's just a big clunky function... could break it up into smaller functions i suppose
 let timer = 0;
 	while ((one.hp > 0) && (two.hp > 0)) {
 		//attack
@@ -235,8 +235,8 @@ let timer = 0;
 			two.hp -= damage;
 		}
 		else { 
-    setTimeout(function(){ console.log(two.name + " dodges nimbly!");}, timer); //miss
-	}
+      setTimeout(function(){ console.log(two.name + " dodges nimbly!");}, timer); //miss
+	 }
   //retaliate
 		if ((RNG(0,20) + two.att) > one.def ) { //scores a hit
 			const damage = RNG(two.damage_min, two.damage_max); //assign damage
@@ -247,21 +247,21 @@ let timer = 0;
 		  setTimeout(function(){ console.log(temp, temp2);}, timer)
     }
    else { 
-   setTimeout(function(){ console.log(one.name + " dodges nimbly!");}, timer) //miss
+      setTimeout(function(){ console.log(one.name + " dodges nimbly!");}, timer) //miss
       const temp = one.hp;
       const temp2 = two.hp
       setTimeout(function(){ console.log(temp, temp2);}, timer)
   }
-timer += 500;
+  timer += 500;
 }
-if (one.hp < 1) {setTimeout(function(){ console.log(one.name + " dies.");}, timer);}
-if (two.hp < 1) {setTimeout(function(){ console.log(two.name + " dies.");}, timer);}
-refresh();
+  if (one.hp < 1) {setTimeout(function(){ console.log(one.name + " dies.");}, timer);}
+  if (two.hp < 1) {setTimeout(function(){ console.log(two.name + " dies.");}, timer);}
+  refresh();
 }
 
 const refresh = () => {
-swashbuckler.hp = 300;
-knight.hp = 400;
+  swashbuckler.hp = 300;
+  knight.hp = 400;
 }
 
 
