ccfd401a355a76670fc017c303cd96d723b0f183
danielhkim92 <danielhkim92@gmail.com>
2017-09-17 19:22:29 -0500

    so far so good

---
+++ hw.js
@@ -0,0 +1,216 @@
+const firstVariable = "Hello World"
+console.log(firstVariable = 1)
+const secondVariable = firstVariable
+console.log(firstVaraible = secondVariable)
+const secondVariable = "hi"
+1
+
+const yourName = "Daniel"
+alert("Hello, my name is " + yourName)
+////
+a<b
+c>d
+'name'=='name'
+a<b<c
+a=a<d
+e === 'Kevin'
+48 =/ '48'
+
+///
+
+
+animal = 'pig'
+if{(animal==='cow')
+	console.log('mooo')}
+	else{
+	console.log("you're not a cow");
+	}
+
+////
+
+const = "22"
+if{(age>15)
+console.log('here are the keys')
+ else if(age<16)
+	console.log("sorry you're too young")}
+
+////
+
+for(let i = 0; i < 11; i++)
+	console.log(i);
+
+for(let i = 10; i<4001; i++)
+
+for(let i = 10; i<4001; i++)
+		if(!(i%2))
+			console.log(i);
+////
+for(let i = 1; i < 101; i++)
+if(i%2)
+	console.log((i) + " is an even number");
+///
+for(let i=0; i <101; i++)
+	if(i%5)
+		console.log("i found a " + (i) + ". HIGH FIVE")
+	if(i%3)
+		console.log("i found a " + (i) + ". THREE IS A CROWD")
+
+////
+
+
+let bankAccount= 0
+	for(let i=1; 1 < 11; i++)
+	bankAccount+= i;
+
+bankAccount=0
+for(let i=0; 1<101; i++)
+	bankAccount+= (i*2);
+////
+
+let sum = 0;
+for (let i = 0; i < 1000; i++);
+	if((i%3) || (i%5))
+		sum += i;
+
+////
+
+
+const quotes = ['poop', 'sleep', 'yawn']
+
+////
+
+ourClass[0]
+ourClass[2] = "World"
+console.log(ourClass[2]);
+
+///
+
+ourClass[2]
+ourClass[2]= "Octocat"
+ourCLass.push('Cloud City')
+
+////
+myArr.push('Egon')
+myArr.pop();
+MyArr.unshift('Bob Marley')
+myArr.shift();
+ourClass.reverse();
+
+///
+if (i < 100){}
+	console.log('little number')}
+	else{
+		console.log('big number')
+	}
+
+////
+
+if (i<5){
+	console.log('little number')
+}
+	else if(5<i<10){
+	console.log('big number')}
+	else{
+		console.log('monkey')
+	}
+
+////
+
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+console.log('Kristyn is rocking that ' + krystynsCloset[2] + ' today!')
+// Kristyn can't find her left shoe. Remove this item from her closet and save it to a variable named kristynShoe.
+kristynsCloset.shift();
+const kristynShoe = 'left shoe'
+// Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+kristynsCloset.pop('raybans')
+
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+kristynsCloset[4]= 'stained knit hat'
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
+thomsCloset[0]
+// In the same way, access one item from Thom's pants array.
+thomsCloset[1][2]
+// Access one item from Thom's accessories array.
+thomsCloset[2][1]
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+console.log("Thom is looking very nice in a" + thomsCloset[0][1] + ", " + thomsCloset[1][2] + "and ") + thomsCloset[2][1]
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+
+thomsCloset[1][3] = 'Footie Pajamas'
+
+const printGreeting = (name) =>
+	console.log("hello there " + name)
+///
+
+const reverseWordOrder = (words) => {
+	return words.split(" ").reverse.join(" ")
+		console.log(reverseWordOrder(words))
+}
+///
+ const calculate = (num1, num2, operation) => {
+ 	if(operation === "add")
+ 		return num1 + num2
+ 	if(operation === 'subt')
+ 		return num1 - num2
+ 	if(operation === 'mult')
+ 		return num1 * num2
+ 	if(operation === 'div')
+ 		return num1/num2
+ 	if(s==='exp')
+ 		return Math.pow(num1, num2);
+ }
+
+///
+const printCool = (name) => {
+
+console.log(printCool(name) + " is cool")
+}
+
+///
+ function calculateCube = (number) => {
+ 	return (number*number*number)
+ 	}
+ 	console.log(calculateCube(number))
+///
+
+
+
+
+
+
+
+
+
+
+
+
