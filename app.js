8974159ee9893f2e3c39a811fd72edddd67bda55
agraham <agraham0925@gmail.com>
2018-03-16 14:19:33 -0500

    completed strings section

---
+++ app.js
@@ -0,0 +1,27 @@
+// CONCEPTUAL QUESTIONS
+//1. How do we assign a value to a variable?
+//2. How do we change the value of a variable?
+//3. How do we assign an existing variable to a new variable?
+
+//STRINGS
+// 1. Create a variable called firstVariable
+let firstVariable;
+// 		assign it the value of a string => "Hello World"
+firstVariable = "Hello World";
+// 		change the value of this variable to a number.
+firstVariable = 69;
+//		store the value of firstVariable into a new variable called secondVariable
+let secondVariable = firstVariable
+// 		change the value of secondVariable to a string.
+secondVariable = "This is my new string";
+// 		What is the value of firstVariable?
+firstVariable = 69;
+
+// 2. Create a variable called yourName and set it equal to your name as a string
+let myName = "Alyssa Graham";
+// 		Write an expression that takes the string "Hello, my name is " and the variable yourName so that it 
+// 		returns a new string with them concatenated.
+console.log("Hello, my name is " + myName);
+
+
+
