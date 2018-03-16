77bcbe83370433e21f47abc426ab18100b3abe48
agraham <agraham0925@gmail.com>
2018-03-16 14:36:20 -0500

    completed conceptual q's

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 // CONCEPTUAL QUESTIONS
 //1. How do we assign a value to a variable?
+//<--- To assign a value you a variable, you start with either let or const, add an assignment operator, and then assign your value. Examples:
+let favoriteFood = "tacos";
+const year = 1989;
+
 //2. How do we change the value of a variable?
+//<--- To change the value, you first need to have declared the variable with let. If so, simply type the variables name, include assignment
+//<---  operator, and set new value. Example:
+favoriteFood = "pad thai";
+
 //3. How do we assign an existing variable to a new variable?
+//<--  Declare your new variable with an assignment operator and then list your existing variable. Example:
+let favoriteAnimal = "dog";
+let myPet = favoriteAnimal;
 
 //STRINGS
 // 1. Create a variable called firstVariable
@@ -22,6 +33,27 @@ let myName = "Alyssa Graham";
 // 		Write an expression that takes the string "Hello, my name is " and the variable yourName so that it 
 // 		returns a new string with them concatenated.
 console.log("Hello, my name is " + myName);
+
+
+// BOOLEANS
+// Using the provided variable definitions, replace the blanks with a mathematical or boolean operator that evaluates the expression to true.
+a < b;
+c > d;
+'Name' ___ 'Name';
+a < b < c;
+a != a < d; //AG comment <-- could also do a != a != d
+e === 'Kevin';
+48  '48';
+
+// THE FARM
+
+
+
+
+
+
+
+
 
 
 
