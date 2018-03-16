2ac629e54a5e9fedad0b3d76396ac22b00eb15ad
Hannah Werman <hannah.werman@gmail.com>
2018-03-16 14:12:45 -0500

    finished conceptual questions

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// THE WEEKEND HOMEWORK 
+
+//conceptual questions
+
+// #1
+// We can assign a value to a variable using const, let, or var. It looks like this:
+const a = "turtle";
+let b = 8;
+var c = ["tiger", "etc."]
+
+// #2
+// We can change the value of variables that are defined using let or var. It looks like this (using the functions defined above):
+b = 22;
+c = ["this", "is", "a", "different", "thing"];
+
+// #3
+// we can assign an existing variable to a new variable like this:
+const d = a;
+let e = c;
+
