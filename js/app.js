1f7baa607eb84c2e9e442f6d7ab35687eb8a6c84
Hannah Werman <hannah.werman@gmail.com>
2018-03-16 14:46:47 -0500

    finsihed multiples of 3 and 5 and will definitely spend all weekend doing Project Euler problems to appease my inner math nerd

--- js/app.js
+++ js/app.js
@@ -159,3 +159,14 @@ for (let i = 1; i <= 100; i ++) {
 }
 
 
+// MULTIPLES OF 3 AND 5 
+
+let sum = 0;
+
+for (let i = 0; i < 1000; i++) {
+	if (i % 3 === 0) {
+		sum += i;
+	} else if (i % 5 === 0) {
+		sum += i;
+	}
+}
