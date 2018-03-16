a93ed2863ba295fbf6d27d10146dc800bf24ae8b
Hannah Werman <hannah.werman@gmail.com>
2018-03-16 14:22:19 -0500

    finished the farm

--- js/app.js
+++ js/app.js
@@ -4,19 +4,19 @@
 
 // #1
 // We can assign a value to a variable using const, let, or var. It looks like this:
-const a = "turtle";
-let b = 8;
-var c = ["tiger", "etc."]
+// const a = "turtle";
+// let b = 8;
+// var c = ["tiger", "etc."]
 
-// #2
-// We can change the value of variables that are defined using let or var. It looks like this (using the functions defined above):
-b = 22;
-c = ["this", "is", "a", "different", "thing"];
+// // #2
+// // We can change the value of variables that are defined using let or var. It looks like this (using the functions defined above):
+// b = 22;
+// c = ["this", "is", "a", "different", "thing"];
 
-// #3
-// we can assign an existing variable to a new variable like this:
-const d = a;
-let e = c;
+// // #3
+// // we can assign an existing variable to a new variable like this:
+// const d = a;
+// let e = c;
 
 
 
@@ -45,8 +45,26 @@ const e = 'Kevin';
 
 a < b
 c > d
-'Name' = 'Name';
+'Name' === 'Name';
 a + b === c;
 a * a === d;
 e === 'Kevin';
 48 == '48'
+
+
+
+// THE FARM 
+
+// 1. write code that will print out 'moooooo' if the variable animal is equal to cow
+let animal = "fish"; 
+
+if (animal === 'cow') {
+	console.log("mooooooo")
+}
+
+// #2  
+if (animal === 'cow') {
+	console.log("mooooooo")
+} else {
+	console.log("Hey! You're not a cow.")
+}
