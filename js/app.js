e95469c9c01b48d683c244a461eac2466c5bd518
Noemi Romero <nlll02@yahoo.com>
2018-03-16 16:02:01 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+//#1 How do we assign a value to a variable?
+you can assign value using let, const, or var 
+
+
+
+//#2 How do we change the value of a variable?
+
+you can change the value of a variable using only let 
+const will not allow you to reasign the value
+
+//#3 How do we assign an existing variable to a new variable?
+let hope = a;
+
+if (a === 1) {
+  let hope = c;
+ 
+}
+
+
+//#4 Create a variable called `firstVariable`.
+  //- assign it the value of a string => `"Hello World"`
+  // - change the value of this variable to a number.
+  // - store the value of `firstVariable` into a new variable called `secondVariable` 
+  // - change the value of `secondVariable` to a string.  
+  // - What is the value of `firstVariable`?
+
+let firstVariable = ("Hello World") => {
+	let secondVariable = firstVariable()
+	
+	}
+	console.log(firstVariable)
+}
+
+
+let firtsV = (a, b)
+ if (a & b === firstV)
+ let secondV = (num)
+secondv === firstV
+
+
+///#5 Create a variable called `yourName` and 
+//set it equal to your name as a string.
+//- Write an expression that takes the string 
+//"Hello, my name is " and the variable `yourName` 
+//so that it returns a new string with them concatenated. 
+//>ex: `Hello, my name is Jean Valjean`
+ let name = "Noemi"
+const yourName = ('') => {
+	console.log(('Hello my name is') + yourName())
+}
+yourName('')
+
+///#6 Using the provided variable definitions, 
+//replace the blanks with a mathematical or 
+//boolean operator that evaluates the expression to true. 
+//:wave:
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+console.log(a < b); //console.log(a < b); true undefined
+// a _ b;
+console.log(c > d); //console.log(c > d); true. undefined
+// c _ d;
+console.log('Name' === 'Name'); //console.log('Name' === 'Name');true undefined
+// 'Name' ___ 'Name';
+console.log((a + b) === c); //console.log((a + b) === c); true
+// a _ b ___ c;
+console.log((a * a) === d);//console.log((a * a) === d); true
+// a _ a ___ d;
+console.log(e === 'Kevin'); //console.log(e === 'Kevin');  true
+// e ___ 'Kevin';
+console.log(48 == '48'); ///console.log(48 == '48'); true
+// 48 ___ '48';
+
+
+
+
+
+
+
+
+
+
+
+
