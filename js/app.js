2632bbe9d276f1abd7566e699fabcbfb6a02c47c
Hannah Werman <hannah.werman@gmail.com>
2018-03-16 14:28:03 -0500

    finished just loop it and drivers ed

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ secondVariable = "This is a string";
 
 // #2
 const yourName = "Hannah";
-console.log("Hello, my name is " + yourName);
+// console.log("Hello, my name is " + yourName);
 
 
 
@@ -58,13 +58,49 @@ e === 'Kevin';
 // 1. write code that will print out 'moooooo' if the variable animal is equal to cow
 let animal = "fish"; 
 
-if (animal === 'cow') {
-	console.log("mooooooo")
-}
+// if (animal === 'cow') {
+// 	console.log("mooooooo")
+// }
 
 // #2  
-if (animal === 'cow') {
-	console.log("mooooooo")
-} else {
-	console.log("Hey! You're not a cow.")
+// if (animal === 'cow') {
+// 	console.log("mooooooo")
+// } else {
+// 	console.log("Hey! You're not a cow.")
+// }
+
+
+// DRIVER'S ED 
+
+// #1
+let age = 20;
+
+// #2
+// if (age >= 16) {
+// 	console.log("Here are the keys")
+// }
+
+// #3
+// if (age >= 16) {
+// 	console.log("Here are the keys")
+// } else {
+// 	console.log("Sorry, youre too young.")
+// }
+
+
+// JUST LOOP IT
+
+// #1
+for (let i = 0; i <= 10; i++) {
+	// console.log(i)
+}
+
+// #2
+for (let i = 10; i <= 4000; i++) {
+	// console.log(i)
+}
+
+// #3
+for (let i = 10; i <= 4000; i += 2) {
+	console.log(i)
 }
