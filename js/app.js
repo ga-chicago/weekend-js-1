dc4b1034fede0c6e36ccfe3d78af8a74a9061d89
Hannah Werman <hannah.werman@gmail.com>
2018-03-16 14:31:35 -0500

    finished lets get even

--- js/app.js
+++ js/app.js
@@ -102,5 +102,23 @@ for (let i = 10; i <= 4000; i++) {
 
 // #3
 for (let i = 10; i <= 4000; i += 2) {
-	console.log(i)
+	// console.log(i)
 }
+
+
+/// LET'S GET EVEN 
+
+// #1
+for (let i = 1; i <= 100; i++) {
+	if (i % 2 === 0) {
+		// console.log(i)
+	}
+}
+
+// #2
+for (let i = 1; i <= 100; i++) {
+	if (i % 2 === 0) {
+		console.log(i + " is an even number")
+	}
+}
+
