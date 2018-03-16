a4d364a0aa24b4cd3f87543b2abe7751a015ea11
Hannah Werman <hannah.werman@gmail.com>
2018-03-16 14:34:17 -0500

    finished give me five

--- js/app.js
+++ js/app.js
@@ -118,7 +118,25 @@ for (let i = 1; i <= 100; i++) {
 // #2
 for (let i = 1; i <= 100; i++) {
 	if (i % 2 === 0) {
-		console.log(i + " is an even number")
+		// console.log(i + " is an even number")
 	}
 }
 
+
+// GIVE ME FIVE 
+
+// #1
+for (let i = 0; i <= 100; i++) {
+	if (i % 5 === 0) {
+		// console.log("I found a " + i + ". High five!")
+	}
+}
+
+// #2
+for (let i = 0; i <= 100; i++) {
+	if (i % 5 === 0) {
+		console.log("I found a " + i + ". High five!")
+	} else if (i % 3 === 0) {
+		console.log("I found a " + i + ". Three is a crowd")
+	}
+}
